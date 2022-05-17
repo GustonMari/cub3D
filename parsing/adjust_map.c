1973c037bede92afa7a899d1dfe2245353adacdf
Gustave MARY <gmary@e3r6p12.clusters.42paris.fr>
2022-05-17 10:17:32 +0200

    clean and finish V1

--- parsing/adjust_map.c
+++ parsing/adjust_map.c
@@ -6,7 +6,7 @@
 /*   By: gmary <gmary@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 17:16:23 by ndormoy           #+#    #+#             */
-/*   Updated: 2022/05/17 09:59:38 by gmary            ###   ########.fr       */
+/*   Updated: 2022/05/17 10:05:43 by gmary            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,49 +71,3 @@ char	**adjust_map(char **map, int nb_line)
 	ft_free_tab_2d(map);
 	return (new_map);
 }
-
-//WARNING
-/* char	**adjust_map(char **map, int nb_line)
-{
-	int		i;
-	int		max_len;
-	char	**new_map;
-
-	i = 0;
-	new_map = NULL;
-	max_len = find_max_lenght(map) - 1;
-	new_map = ft_calloc(sizeof(char *), nb_line + 1);
-	if (!new_map)
-		return (ft_free_tab_2d(map));
-	new_map[nb_line] = NULL;
-	if (!new_map)
-		return (NULL);
-	while (i < nb_line)
-	{
-		if (ft_strlen(map[i]) != max_len)
-		{
-			new_map[i] = ft_strdup_less(map[i], 1);
-			if (!new_map[i])
-				return (adjust_map_free(new_map, map));
-			while (ft_strlen(new_map[i]) < max_len)
-			{
-				new_map[i] = ft_strjoin_free(new_map[i], " ", 1);
-				if (!new_map[i])
-					return (adjust_map_free(new_map, map));
-			}
-			new_map[i] = ft_strjoin_free(new_map[i], "\n", 1);
-			if (!new_map[i])
-				return (adjust_map_free(new_map, map));
-		}
-		else
-		{
-			new_map[i] = ft_strdup(map[i]);
-			if (!new_map)
-				return (NULL);
-		}
-		i++;
-	}
-	ft_free_tab_2d(map);
-	print_tab_2d(new_map);
-	return (new_map);
-} */
